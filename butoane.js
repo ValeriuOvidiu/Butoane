3b3b154612559c004fb919d3c5a317b5a6985b1e
ValeriuOvidiu <126098277+ValeriuOvidiu@users.noreply.github.com>
2023-03-28 18:56:02 +0300

    Update butoane.js

--- butoane.js
+++ butoane.js
@@ -1,14 +1,12 @@
-function createf() {
-  let a = 0;
-  var num = prompt("numarul de butoane ")
-  var num2 = prompt("butonul castigator ")
-  for (let i = 1; i <= num; ++i) {
-    a += i;
+function createButtons() {
+  var numberOfButtons = prompt("numarul de butoane ")
+  var theWinner = prompt("butonul castigator ")
+  for (let i = 1; i <= numberOfButtons; ++i) {
     var button = document.createElement("button");
     button.innerHTML = "Push " + i;
-    var body = document.getElementsByTagName("body")[0];
+    var body = document.getElementById("buttonsOutput");
     body.appendChild(button);
-    if (i == num2) {
+    if (i == theWinner) {
       button.addEventListener("click", function () {
         alert("ai apasat butonul castigator");
       });
